3f581d062a4f29d5942848187475ca64b0475da4
atnum <anupmridha007@gmail.com>
2018-01-28 14:35:58 +0530

    first commit

---
+++ src/main/java/com/anup/SpringRestClientApplication.java
@@ -0,0 +1,26 @@
+package com.anup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.anup.restclient.RestClient;
+
+@SpringBootApplication
+public class SpringRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientApplication.class, args);
+
+		// Getting Products from Service
+		 RestClient.getProducts();
+
+		// Posting products
+		// RestClient.createProduct();
+
+		// Updating products
+		// RestClient.updateProduct();
+
+		// Deleting products
+		// RestClient.deleteProduct();
+	}
+}
---
+++ src/main/java/com/anup/model/Product.java
@@ -0,0 +1,57 @@
+package com.anup.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Product implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4225389340280605521L;
+
+	private int productId;
+	private String name;
+	private String description;
+	private BigDecimal price;
+
+	public Product() {
+	}
+
+	public Product(String name, String description, BigDecimal price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", name=" + name + ", description=" + description + ", price="
+				+ price + "]";
+	}
+
+}
